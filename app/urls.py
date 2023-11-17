c7e2f5cf81fa76ef96530267b41745d505c10c7d
XurlimanKalmuratova <xurlimankalmuratova@gmail.com>
2023-11-17 19:26:41 +0500

    last

--- app/urls.py
+++ app/urls.py
@@ -6,9 +6,12 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
-    path('contact/', contact, name='contact'),
-    path('project/', views.Project.as_view(), name='project'),
+    path('project/',project, name='project'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('registration/', registration, name='registration'),
+    path('create/', create, name='create'),
+    path('project/<int:pk>/', project_detail, name='project_detail'),
+    path('project/<pk>/update/', update, name='update'),
+    path('project/<pk>/delete/', delete, name='delete'),
 
 ]    
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from .models import Projects
 from django.http import HttpResponse
 from django.views.generic import ListView
 from .forms import LoginForm
-
+from .forms import ProjectsForm
 
 def home(request):
     return render(request, 'home.html')
@@ -13,8 +13,6 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def contact(request):
-    return render(request, 'contact.html')
 
 def login(request):
     if request.method == 'POST':
@@ -30,7 +28,7 @@ def login(request):
                 if user.is_active:
                     login(request, user)
                     #return HttpResponse('Authenticated succesfully.')
-                    return HttpResponseRedirect('project/')
+                    return HttpResponseRedirect('/')
                 
                 
             else:
@@ -49,12 +47,53 @@ def login(request):
 def registration(request):
     return render(request, 'registration.html')
 
+def project(request):
+    projects = Projects.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project.html', context)
 
-class Project(ListView):
-    template_name = 'project.html'
-    model = Projects
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['projects'] = Projects.objects.all()
-        return context
+
+def project_detail(request, pk):
+    project = Projects.objects.get(pk=pk)
+    context = {
+        'project' : project
+    }
+    return render(request, 'project_detail.html', context)
+
+
+
+def create(request):
+    if request.method == 'POST':
+        form = ProjectsForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(to='project')
+    else:
+        form = ProjectsForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'create.html', context)
+
+
+def update(request, pk):
+    form = ProjectsForm()
+    if request.method == 'POST':
+        form = ProjectsForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(to='project')
+    context = {
+        'form': form
+    }
+    return render(request, 'update.html', context)
+
+
+def delete(request, pk):
+    projects = Projects.objects.get(pk=pk)
+    projects.delete()
+    return redirect(to='project')
